f72dcd4af7b5533db7d285eaaa5d5d5f654a1635
NikkoAvardes <11460@holbertonstudents.com>
2025-08-14 15:56:02 +0200

    Refactor main function to improve command execution logic and add print_env function to display environment variables

--- main.c
+++ main.c
@@ -22,9 +22,23 @@ int main(int argc, char **argv)
 
 		if (line == NULL)
 			continue;
+			
+		if (line[0] == '\0')
+		{
+			free(line);
+			continue;
+		}
+		
 		args = parse_line(line);
-
-		execute(args, argv[0]);
+		if (args && args[0])
+		{
+			if (strcmp(args[0], "env") == 0)
+				print_env();
+			else
+				execute(args, argv[0]);
+		}
+		
+		free(args);
 		free(line);
 	}
 	return (0);
