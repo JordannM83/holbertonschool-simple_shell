ff1bf490a174bc99af8d9aabf38b841256085d71
NikkoAvardes <11460@holbertonstudents.com>
2025-08-14 10:49:30 +0200

    Améliore la gestion des erreurs dans shell_exit et refactorise la fonction execute pour simplifier le code et améliorer la lisibilité.

--- src/builtin.c
+++ src/builtin.c
@@ -7,6 +7,8 @@
 
 void *shell_exit(char *line, ssize_t nread)
 {
+	char *argc = NULL;
+
 	if (nread == -1)
 	{
 		if (feof(stdin))
@@ -18,9 +20,9 @@ void *shell_exit(char *line, ssize_t nread)
 		print_error("Get line error");
 		exit(1);
 	}
-	else if ()
+	else if (strcmp(&argc[0], "exit") == 0)
 	{
-		/* code */
+		exit(0);
 	}
 
 	return (line);
--- src/execute.c
+++ src/execute.c
@@ -1,44 +1,22 @@
 #include "shell.h"
-#include <stdio.h>
-/**
- * Execute a command.
- * This function creates a child process to execute the command
- * using execve. It handles errors by printing them to stderr.
- * @args: An array of arguments for the command, including the command itself.
- * Returns 0 on success, or -1 on failure.
- * Note: The caller is responsible for freeing the args array.
- * execute - Executes a command in a child process.
- * @args: The arguments for the command.
- * Return: 0 on success, -1 on failure.
- */
 
+/**
+ * execute - Execute a command in a child process
+ * @args: Array of arguments for the command
+ *
+ * Description: Creates a child process to execute the command using execve.
+ * Handles errors by printing them to stderr.
+ * Return: 0 on success, -1 on failure
+ */
 int execute(char **args)
 {
+	pid_t pid;
 	int i;
 
 	if (args && args[0])
 	{
-			pid_t pid = fork();
-	if (args && args[0])
-	{
-		pid_t pid = fork();
+		pid = fork();
 
-			if (pid == 0)
-			{
-				execve(args[0], args, environ);
-				print_error("execve");
-				exit(EXIT_FAILURE);
-			}
-			else if (pid > 0)
-			{
-				wait(NULL);
-			}
-			else
-			{
-				print_error("fork");
-			}
-			for (i = 0; args[i]; i++)
-				free(args[i]);
 		if (pid == 0)
 		{
 			execve(args[0], args, environ);
@@ -52,12 +30,10 @@ int execute(char **args)
 		else
 		{
 			print_error("fork");
+			return (-1);
 		}
 		for (i = 0; args[i]; i++)
 			free(args[i]);
-
-			free(args);
-	}
 		free(args);
 	}
 	return (0);
