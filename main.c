a91ee9be776c99b1b14fde2b46e7e54ef665663a
JordannM83 <11457@holbertonstudents.com>
2025-08-17 08:25:06 +0200

    Refactor shell implementation: remove unused functions and consolidate main loop logic

--- main.c
+++ main.c
@@ -1,41 +1,18 @@
-
 #include "shell.h"
 
 /**
- * main - Entry point of the shell program
- * @argc: Number of arguments
- * @argv: Array of arguments
+ * main - Entry point for the simple shell
+ * @argc: Argument count
+ * @argv: Argument vector
  *
- * Return: Always 0 (Success)
+ * Return: 0 on success
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	char *line;
-	char **args;
-
 	(void)argc;
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-			prompt();
-		line = read_line();
 
-		if (line[0] == '\0')
-		{
-			free(line);
-			continue;
-		}
-		args = parse_line(line);
-		if (args && args[0])
-		{
-			if (strcmp(args[0], "env") == 0)
-				print_env();
-			else
-				execute(args, argv[0]);
-		}
-		
-		free(line);
-		free(args);
-	}
+	/* Run the shell loop with program name */
+	shell_loop(argv[0]);
+
 	return (0);
 }
---
+++ shell.c
@@ -0,0 +1,156 @@
+#include "shell.h"
+
+/**
+ * print_prompt - Print the shell prompt
+ */
+void print_prompt(void)
+{
+	if (isatty(STDIN_FILENO))
+		printf("($) ");
+}
+
+/**
+ * read_line - Read a line of input from stdin
+ *
+ * Return: The line read from stdin
+ */
+char *read_line(void)
+{
+	char *line = NULL;
+	size_t bufsize = 0;
+	ssize_t characters;
+
+	characters = getline(&line, &bufsize, stdin);
+
+	if (characters == -1)
+	{
+		if (feof(stdin))
+		{
+			if (isatty(STDIN_FILENO))
+				printf("\n");
+			free(line);
+			exit(0);
+		}
+		else
+		{
+			perror("getline");
+			free(line);
+			exit(1);
+		}
+	}
+
+	return (line);
+}
+
+/**
+ * split_line - Split a line into tokens
+ * @line: The line to split
+ *
+ * Return: Array of tokens
+ */
+char **split_line(char *line)
+{
+	int bufsize = 64;
+	int position = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token;
+
+	if (!tokens)
+	{
+		fprintf(stderr, "allocation error\n");
+		exit(1);
+	}
+
+	token = strtok(line, " \t\r\n\a");
+	while (token != NULL)
+	{
+		tokens[position] = token;
+		position++;
+
+		if (position >= bufsize)
+		{
+			bufsize += 64;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+			{
+				fprintf(stderr, "allocation error\n");
+				exit(1);
+			}
+		}
+
+		token = strtok(NULL, " \t\r\n\a");
+	}
+	tokens[position] = NULL;
+	return (tokens);
+}
+
+/**
+ * execute_command - Execute a command
+ * @args: Array of arguments
+ * @prog_name: Program name for error messages
+ *
+ * Return: 1 to continue, 0 to exit
+ */
+int execute_command(char **args, char *prog_name)
+{
+	pid_t pid;
+	int status;
+
+	if (args[0] == NULL)
+	{
+		/* Empty command */
+		return (1);
+	}
+
+	/* Handle exit command */
+	if (strcmp(args[0], "exit") == 0)
+	{
+		return (0);
+	}
+
+	pid = fork();
+	if (pid == 0)
+	{
+		/* Child process */
+		if (execve(args[0], args, environ) == -1)
+		{
+			fprintf(stderr, "%s: 1: %s: not found\n", prog_name, args[0]);
+		}
+		exit(127);
+	}
+	else if (pid < 0)
+	{
+		/* Error forking */
+		perror("fork");
+	}
+	else
+	{
+		/* Parent process */
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+
+	return (1);
+}
+
+/**
+ * shell_loop - Main shell loop
+ * @prog_name: Program name for error messages
+ */
+void shell_loop(char *prog_name)
+{
+	char *line;
+	char **args;
+	int status;
+
+	do {
+		print_prompt();
+		line = read_line();
+		args = split_line(line);
+		status = execute_command(args, prog_name);
+
+		free(line);
+		free(args);
+	} while (status);
+}
