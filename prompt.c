db7470e84e9adca0b4c4f6cb2036ff144b1861d3
JordannM83 <11457@holbertonstudents.com>
2025-08-17 09:12:35 +0200

    Refactor prompt function: update documentation and remove unnecessary condition

--- prompt.c
+++ prompt.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * prompt - Displays the shell prompt.
+ * prompt - Print the shell prompt
  */
-
 void prompt(void)
 {
-	printf("#cisfun$ ");
+	if (isatty(STDIN_FILENO))
+		printf("#cisfun$ ");
 }
